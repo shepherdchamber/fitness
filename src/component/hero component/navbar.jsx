f453881bba8b2bc3f05060e10e6c9ae7e1b46c8d
shepherdchamber <adebanjodavid2007@gmail.com>
2025-04-04 13:21:36 +0100

    navbar

--- src/component/hero component/navbar.jsx
+++ src/component/hero component/navbar.jsx
@@ -1,6 +1,12 @@
 import '../../styles/navbar.css';
+import icon from '../../../src/assets/image/menu.png';
+import { useState } from 'react';
 
 export function Navbar() {
+  const [show, setShow] = useState(true);
+  const toggleShow = () => {
+    setShow(!show);
+  };
   return (
     <div className='nav-bar'>
       <div className='logo-container'>
@@ -8,13 +14,16 @@ export function Navbar() {
         <p className='trafalgar'>Trafalgar</p>
       </div>
       <div className='top-nav'>
-        <ul>
+        <ul className={show ? 'hide-menu' : 'show-menu'}>
           <li>About us</li>
           <li>testimonials</li>
           <li>apps</li>
           <li>find doctor</li>
           <li>home</li>
         </ul>
+      </div>
+      <div className='icon'>
+        <img onClick={toggleShow} src={icon} alt='' />
       </div>
     </div>
   );
